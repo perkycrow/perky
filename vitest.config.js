c389186f425b9ae3ba3e35b07cfeb5f9d175626c
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2025-05-04 09:47:52 +0200

    updated vitest.config.js for jsdom

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,10 @@
 import {defineConfig} from 'vitest/config'
 
+
 export default defineConfig({
     test: {
         globals: true,
-        environment: 'node',
+        environment: 'jsdom',
         include: ['**/*.test.js', '**/*.spec.js']
     }
 })
