77e7682c3cb71ab393c10166ce959d00936c06ca
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2025-05-11 15:48:49 +0200

    added examples index

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import {defineConfig} from 'vite'
+import path from 'path'
 
 export default defineConfig({
+    root: './',
+    publicDir: false,
+    server: {
+        port: 3000,
+        open: '/examples/'
+    },
     build: {
-
+        outDir: 'dist',
+        assetsDir: 'assets'
+    },
+    resolve: {
+        alias: {
+            perky: path.resolve(__dirname, './')
+        }
     }
 })
