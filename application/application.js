da995eefc52ce8d838dc0517503c37a217cd2ba6
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2025-05-11 18:15:13 +0200

    updated application use global InputObserver

--- application/application.js
+++ application/application.js
@@ -9,13 +9,13 @@ import {loaders} from './loaders'
 
 export default class Application extends Engine {
 
-    constructor (manifest = {}) {
-        super(manifest)
+    constructor (params = {}) {
+        super(params)
 
         this.loaders = new Registry(loaders)
+        this.inputObserver = InputObserver.global
 
         this.registerModule('perkyView', new PerkyView({className: 'perky-application'}))
-        this.registerModule('inputObserver', new InputObserver())
         this.registerModule('inputMapper', new InputMapper({inputObserver: this.inputObserver}))
         this.registerModule('sourceManager', new SourceManager({
             loaders: this.loaders,
