f419f47eb8d4a6e27380ac2ee8dacd6176b21d2d
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2025-06-15 16:37:36 +0200

    simplified keyboard_device

--- input/input_devices/keyboard_device.js
+++ input/input_devices/keyboard_device.js
@@ -1,262 +1,89 @@
 import InputDevice from '../input_device'
-import KeyControl from '../input_controls/key_control'
+import ButtonControl from '../input_controls/button_control'
 
 
 export default class KeyboardDevice extends InputDevice {
 
-    static methods = [
-        'isKeyPressed',
-        'isKeyModifierPressed',
-        'getPressedKeys',
-        'getPressedKeyModifiers'
-    ]
-
-    static events = [
-        'keydown',
-        'keyup'
-    ]
-
-
     constructor (params = {}) {
-        super(params)
+        super({
+            container: params.container || window,
+            name: params.name || 'KeyboardDevice'
+        })
 
-        this.pressedKeys = {}
-        this.pressedModifiers = {}
+        this.keydownListener = this.#handleKeydown.bind(this)
+        this.keyupListener = this.#handleKeyup.bind(this)
+        this.blurListener = this.#handleBlur.bind(this)
     }
 
 
-    observe () {
-        return observe(this)
-    }
-
-
-    unobserve () {
-        return unobserve(this)
-    }
-
-
-    isPressed (code) {
-        return this.isKeyPressed(code) || this.isKeyModifierPressed(code)
-    }
-
-
-    isKeyPressed (code) {
-        return code in this.pressedKeys
-    }
-
-
-    isKeyModifierPressed (code) {
-        return code in this.pressedModifiers
-    }
-
-
-    getPressedKeys () {
-        return Object.keys(this.pressedKeys)
-    }
-
-
-    getPressedKeyModifiers () {
-        return Object.keys(this.pressedModifiers)
-    }
-
-
-    getOrCreateKeyControl (code, key = null) {
-        let keyControl = this.getControl(code)
-        
-        if (!keyControl) {
-            keyControl = new KeyControl({
-                device: this,
-                name: code,
-                displayName: getKeyDisplayName(code, key)
-            })
-            this.addControl(code, keyControl)
+    start () {
+        if (!super.start()) {
+            return false
         }
-        
-        return keyControl
-    }
 
-}
-
-
-function observe (device) {
-
-    if (device.keyboardListeners) {
-        return false
-    }
-
-    const listeners = {
-        keydown (event) {
-            const modifiers = getModifiers(event)
-            const keyState = createKeyState(event, modifiers)
-
-            updateModifiers(device, modifiers)
-            device.pressedKeys[event.code] = keyState
-
-            const keyControl = device.getOrCreateKeyControl(event.code, event.key)
-            keyControl.press()
-
-            device.emit('keydown', keyState)
-        },
-        keyup (event) {
-            const modifiers = getModifiers(event)
-            const keyState = createKeyState(event, modifiers)
-
-            delete device.pressedKeys[event.code]
-            updateModifiers(device, modifiers)
-            
-            const keyControl = device.getOrCreateKeyControl(event.code, event.key)
-            keyControl.release()
-            
-            if ((event.code === 'MetaLeft' || event.code === 'MetaRight') && !modifiers.Meta) {
-                clearNonModifierKeys(device)
-            }
-            
-            device.emit('keyup', keyState)
-        },
-        blur () {
-            for (const control of device.getAllControls()) {
-                if (control.release) {
-                    control.release()
-                }
-            }
-            clearAllKeys(device)
-            device.emit('blur')
-        }
-    }
-
-    const {container} = device
-
-    container.addEventListener('keydown', listeners.keydown)
-    container.addEventListener('keyup', listeners.keyup)
-    window.addEventListener('blur', listeners.blur)
-
-    device.keyboardListeners = listeners
-
-    return true
-}
-
-
-function unobserve (device) {
-    const listeners = device.keyboardListeners
-
-    if (listeners) {
-        const {container} = device
-
-        container.removeEventListener('keydown', listeners.keydown)
-        container.removeEventListener('keyup', listeners.keyup)
-        window.removeEventListener('blur', listeners.blur)
-
-        delete device.keyboardListeners
+        this.container.addEventListener('keydown', this.keydownListener)
+        this.container.addEventListener('keyup', this.keyupListener)
+        this.container.addEventListener('blur', this.blurListener)
 
         return true
     }
 
-    return false
-}
 
+    stop () {
+        if (!super.stop()) {
+            return false
+        }
 
-function createKeyState (event, modifiers = {}) {
-    return {
-        code:      event.code,
-        key:       event.key,
-        repeat:    event.repeat,
-        modifiers: modifiers
+        this.container.removeEventListener('keydown', this.keydownListener)
+        this.container.removeEventListener('keyup', this.keyupListener)
+        this.container.removeEventListener('blur', this.blurListener)
+
+        return true
     }
-}
 
 
-function updateModifiers (device, modifiers) {
-    device.pressedModifiers = {}
+    dispose () {
+        this.stop()
+        return super.dispose()
+    }
 
-    for (const key in modifiers) {
-        if (modifiers[key]) {
-            device.pressedModifiers[key] = true
+
+    #handleKeydown (event) {
+        const keyName = getKeyName(event)
+        const control = this.findOrCreateControl(ButtonControl, {
+            name: keyName
+        })
+
+        if (!control.isPressed) {
+            control.press()
         }
     }
-}
 
 
-function clearNonModifierKeys (device) {
-    const modifierCodes = [
-        'AltLeft', 'AltRight',
-        'ControlLeft', 'ControlRight', 
-        'MetaLeft', 'MetaRight',
-        'ShiftLeft', 'ShiftRight',
-        'CapsLock', 'NumLock'
-    ]
-    
-    for (const code in device.pressedKeys) {
-        if (!modifierCodes.includes(code)) {
-            delete device.pressedKeys[code]
-            const control = device.getControl(code)
-            if (control && control.release) {
+    #handleKeyup (event) {
+        const keyName = getKeyName(event)
+        const control = this.getControl(keyName)
+
+        if (control && control.isPressed) {
+            control.release()
+        }
+    }
+
+
+    #handleBlur () {
+        this.pressedNames.forEach(keyName => {
+            const control = this.getControl(keyName)
+            if (control && control.isPressed) {
                 control.release()
             }
-        }
+        })
     }
+
+
 }
 
 
-function clearAllKeys (device) {
-    device.pressedKeys = {}
-    device.pressedModifiers = {}
+function getKeyName (event) {
+    return event.code
 }
 
-
-function getKeyDisplayName (code, key = null) {
-    const specialKeys = {
-        Space: 'Space',
-        Enter: 'Enter',
-        Escape: 'Esc',
-        Tab: 'Tab',
-        Backspace: 'Backspace',
-        Delete: 'Delete',
-        ArrowUp: '↑',
-        ArrowDown: '↓',
-        ArrowLeft: '←',
-        ArrowRight: '→',
-        ShiftLeft: 'Left Shift',
-        ShiftRight: 'Right Shift',
-        ControlLeft: 'Left Ctrl',
-        ControlRight: 'Right Ctrl',
-        AltLeft: 'Left Alt',
-        AltRight: 'Right Alt',
-        MetaLeft: 'Left Cmd',
-        MetaRight: 'Right Cmd'
-    }
-
-    if (specialKeys[code]) {
-        return specialKeys[code]
-    }
-
-    if (key && key.length === 1 && key !== ' ') {
-        return key.toUpperCase()
-    }
-
-    return code.replace('Key', '').replace('Digit', '')
-}
-
-
-const MODIFIERS = [
-    'Alt',
-    'Control',
-    'Meta',
-    'Shift',
-    'CapsLock',
-    'NumLock',
-    'AltGraph'
-]
-
-
-function getModifiers (event) {
-    const modifiers = {}
-
-    for (const key of MODIFIERS) {
-        const modifier = event.getModifierState(key)
-        if (modifier) {
-            modifiers[key] = true
-        }
-    }
-
-    return modifiers
-}
--- input/input_devices/keyboard_device.test.js
+++ input/input_devices/keyboard_device.test.js
@@ -1,269 +1,185 @@
 import KeyboardDevice from './keyboard_device'
-import KeyControl from '../input_controls/key_control'
+import InputDevice from '../input_device'
+import ButtonControl from '../input_controls/button_control'
 import {vi} from 'vitest'
 
-
 describe(KeyboardDevice, () => {
+    let device
+    let mockContainer
 
-    let keyboardDevice
 
     beforeEach(() => {
-        keyboardDevice = new KeyboardDevice()
-        keyboardDevice.start()
-    })
+        mockContainer = {
+            addEventListener: vi.fn(),
+            removeEventListener: vi.fn()
+        }
 
-    afterEach(() => {
-        keyboardDevice.stop()
+        device = new KeyboardDevice({
+            container: mockContainer,
+            name: 'testKeyboard'
+        })
     })
 
 
     test('constructor', () => {
-        expect(keyboardDevice.pressedKeys).toEqual({})
-        expect(keyboardDevice.pressedModifiers).toEqual({})
-        expect(keyboardDevice.name).toBe('KeyboardDevice')
-        expect(keyboardDevice.controls).toBeInstanceOf(Map)
+        expect(device).toBeInstanceOf(InputDevice)
+        expect(device.name).toBe('testKeyboard')
+        expect(device.container).toBe(mockContainer)
+        expect(device.keydownListener).toBeInstanceOf(Function)
+        expect(device.keyupListener).toBeInstanceOf(Function)
+        expect(device.blurListener).toBeInstanceOf(Function)
     })
 
 
-    test('static properties', () => {
-        expect(KeyboardDevice.methods).toContain('isKeyPressed')
-        expect(KeyboardDevice.methods).toContain('isKeyModifierPressed')
-        expect(KeyboardDevice.methods).toContain('getPressedKeys')
-        expect(KeyboardDevice.methods).toContain('getPressedKeyModifiers')
-        expect(KeyboardDevice.events).toContain('keydown')
-        expect(KeyboardDevice.events).toContain('keyup')
+    test('constructor with default params', () => {
+        const defaultDevice = new KeyboardDevice()
+        expect(defaultDevice.name).toBe('KeyboardDevice')
+        expect(defaultDevice.container).toBe(window)
     })
 
 
-    test('getOrCreateKeyControl creates new control', () => {
-        const control = keyboardDevice.getOrCreateKeyControl('KeyA', 'a')
-        
-        expect(control).toBeInstanceOf(KeyControl)
-        expect(control.name).toBe('KeyA')
-        expect(control.displayName).toBe('A')
-        expect(control.device).toBe(keyboardDevice)
-        expect(keyboardDevice.getControl('KeyA')).toBe(control)
+    test('start attaches event listeners', () => {
+        device.start()
+
+        expect(mockContainer.addEventListener).toHaveBeenCalledWith('keydown', device.keydownListener)
+        expect(mockContainer.addEventListener).toHaveBeenCalledWith('keyup', device.keyupListener)
+        expect(mockContainer.addEventListener).toHaveBeenCalledWith('blur', device.blurListener)
+        expect(mockContainer.addEventListener).toHaveBeenCalledTimes(3)
     })
 
 
-    test('getOrCreateKeyControl returns existing control', () => {
-        const control1 = keyboardDevice.getOrCreateKeyControl('KeyB', 'b')
-        const control2 = keyboardDevice.getOrCreateKeyControl('KeyB', 'b')
-        
-        expect(control1).toBe(control2)
-        expect(keyboardDevice.controls.size).toBe(1)
+    test('stop removes event listeners', () => {
+        device.start()
+        device.stop()
+
+        expect(mockContainer.removeEventListener).toHaveBeenCalledWith('keydown', device.keydownListener)
+        expect(mockContainer.removeEventListener).toHaveBeenCalledWith('keyup', device.keyupListener)
+        expect(mockContainer.removeEventListener).toHaveBeenCalledWith('blur', device.blurListener)
+        expect(mockContainer.removeEventListener).toHaveBeenCalledTimes(3)
     })
 
 
-    test('getOrCreateKeyControl with special keys', () => {
-        const spaceControl = keyboardDevice.getOrCreateKeyControl('Space', ' ')
-        const enterControl = keyboardDevice.getOrCreateKeyControl('Enter', 'Enter')
-        const arrowControl = keyboardDevice.getOrCreateKeyControl('ArrowUp', 'ArrowUp')
-        
-        expect(spaceControl.displayName).toBe('Space')
-        expect(enterControl.displayName).toBe('Enter')
-        expect(arrowControl.displayName).toBe('↑')
+    test('dispose calls stop', () => {
+        const stopSpy = vi.spyOn(device, 'stop')
+        device.dispose()
+        expect(stopSpy).toHaveBeenCalled()
     })
 
 
-    test('observe and unobserve', () => {
-        const testKeyboardDevice = new KeyboardDevice()
+
+    test('keydown event creates and presses control', () => {
+        device.start()
+
+        const keydownListener = mockContainer.addEventListener.mock.calls
+            .find(call => call[0] === 'keydown')[1]
         
-        expect(testKeyboardDevice.keyboardListeners).toBeUndefined()
+        const event = {code: 'KeyA'}
+        keydownListener(event)
         
-        testKeyboardDevice.observe()
-        expect(testKeyboardDevice.keyboardListeners).toBeDefined()
-        expect(typeof testKeyboardDevice.keyboardListeners.keydown).toBe('function')
-        expect(typeof testKeyboardDevice.keyboardListeners.keyup).toBe('function')
-        
-        testKeyboardDevice.unobserve()
-        expect(testKeyboardDevice.keyboardListeners).toBeUndefined()
+        const control = device.getControl('KeyA')
+        expect(control).toBeInstanceOf(ButtonControl)
+        expect(control.isPressed).toBe(true)
+        expect(device.isPressed('KeyA')).toBe(true)
     })
 
 
-    test('keydown event creates and updates control', () => {
-        const listener = vi.fn()
-        keyboardDevice.on('keydown', listener)
-
-        const event = new KeyboardEvent('keydown', {
-            code: 'KeyA',
-            key: 'a'
-        })
-        window.dispatchEvent(event)
-
-        expect(listener).toHaveBeenCalled()
-        const keyState = listener.mock.calls[0][0]
-        expect(keyState.code).toBe('KeyA')
-        expect(keyState.key).toBe('a')
-        expect(keyState.repeat).toBe(false)
+    test('keydown event does not press already pressed control', () => {
+        device.start()
         
-        expect(keyboardDevice.isKeyPressed('KeyA')).toBe(true)
+        const keydownListener = mockContainer.addEventListener.mock.calls
+            .find(call => call[0] === 'keydown')[1]
         
-        const control = keyboardDevice.getControl('KeyA')
-        expect(control).toBeInstanceOf(KeyControl)
-        expect(control.isPressed()).toBe(true)
-        expect(control.getValue()).toBe(1)
+        // First keydown
+        const event = {code: 'KeyA'}
+        keydownListener(event)
+        
+        const control = device.getControl('KeyA')
+        const pressSpy = vi.spyOn(control, 'press')
+        
+        // Second keydown should not press again
+        keydownListener(event)
+        expect(pressSpy).not.toHaveBeenCalled()
     })
 
 
-    test('keyup event updates control', () => {
-        const listener = vi.fn()
-        keyboardDevice.on('keyup', listener)
-
-        window.dispatchEvent(new KeyboardEvent('keydown', {
-            code: 'KeyB',
-            key: 'b'
-        }))
+    test('keyup event releases control', () => {
+        device.start()
         
-        window.dispatchEvent(new KeyboardEvent('keyup', {
-            code: 'KeyB',
-            key: 'b'
-        }))
-
-        expect(listener).toHaveBeenCalled()
-        const keyState = listener.mock.calls[0][0]
-        expect(keyState.code).toBe('KeyB')
-        expect(keyState.key).toBe('b')
+        const keydownListener = mockContainer.addEventListener.mock.calls
+            .find(call => call[0] === 'keydown')[1]
+        const keyupListener = mockContainer.addEventListener.mock.calls
+            .find(call => call[0] === 'keyup')[1]
         
-        expect(keyboardDevice.isKeyPressed('KeyB')).toBe(false)
+        // First press the key
+        const event = {code: 'KeyA'}
+        keydownListener(event)
+        expect(device.isPressed('KeyA')).toBe(true)
         
-        const control = keyboardDevice.getControl('KeyB')
-        expect(control).toBeInstanceOf(KeyControl)
-        expect(control.isPressed()).toBe(false)
-        expect(control.getValue()).toBe(0)
+        // Then release it
+        keyupListener(event)
+        expect(device.isPressed('KeyA')).toBe(false)
     })
 
 
-    test('blur event releases all controls', () => {
-        window.dispatchEvent(new KeyboardEvent('keydown', {code: 'KeyA', key: 'a'}))
-        window.dispatchEvent(new KeyboardEvent('keydown', {code: 'KeyB', key: 'b'}))
+    test('keyup event does nothing for non-existent control', () => {
+        device.start()
         
-        const controlA = keyboardDevice.getControl('KeyA')
-        const controlB = keyboardDevice.getControl('KeyB')
+        const keyupListener = mockContainer.addEventListener.mock.calls
+            .find(call => call[0] === 'keyup')[1]
         
-        expect(controlA.isPressed()).toBe(true)
-        expect(controlB.isPressed()).toBe(true)
-        
-        window.dispatchEvent(new Event('blur'))
-
-        expect(controlA.isPressed()).toBe(false)
-        expect(controlB.isPressed()).toBe(false)
-        expect(keyboardDevice.pressedKeys).toEqual({})
+        // Try to release a key that was never pressed
+        const event = {code: 'KeyA'}
+        expect(() => keyupListener(event)).not.toThrow()
+        expect(device.getControl('KeyA')).toBeUndefined()
     })
 
 
-    test('key modifiers', () => {
-        const listener = vi.fn()
-        keyboardDevice.on('keydown', listener)
-
-        const event = new KeyboardEvent('keydown', {
-            code: 'ShiftLeft',
-            key: 'Shift',
-            shiftKey: true
-        })
-
-        Object.defineProperty(event, 'getModifierState', {
-            value: (key) => key === 'Shift'
-        })
+    test('blur event releases all pressed keys', () => {
+        device.start()
         
-        window.dispatchEvent(event)
-
-        expect(listener).toHaveBeenCalled()
-        const keyState = listener.mock.calls[0][0]
-        expect(keyState.modifiers.Shift).toBe(true)
+        const keydownListener = mockContainer.addEventListener.mock.calls
+            .find(call => call[0] === 'keydown')[1]
+        const blurListener = mockContainer.addEventListener.mock.calls
+            .find(call => call[0] === 'blur')[1]
         
-        expect(keyboardDevice.isKeyModifierPressed('Shift')).toBe(true)
-        expect(keyboardDevice.getPressedKeyModifiers()).toContain('Shift')
+        // Press multiple keys
+        keydownListener({code: 'KeyA'})
+        keydownListener({code: 'KeyB'})
+        expect(device.isPressed('KeyA')).toBe(true)
+        expect(device.isPressed('KeyB')).toBe(true)
         
-        const shiftControl = keyboardDevice.getControl('ShiftLeft')
-        expect(shiftControl.displayName).toBe('Left Shift')
-        expect(shiftControl.isPressed()).toBe(true)
+        // Blur should release all
+        blurListener()
+        expect(device.isPressed('KeyA')).toBe(false)
+        expect(device.isPressed('KeyB')).toBe(false)
     })
 
 
-    test('getPressedKeys', () => {
-        window.dispatchEvent(new KeyboardEvent('keydown', {
-            code: 'KeyA',
-            key: 'a'
-        }))
+    test('integration test - full key press cycle', () => {
+        const controlPressedListener = vi.fn()
+        const controlReleasedListener = vi.fn()
         
-        window.dispatchEvent(new KeyboardEvent('keydown', {
-            code: 'KeyB',
-            key: 'b'
-        }))
-
-        const pressedKeys = keyboardDevice.getPressedKeys()
-        expect(pressedKeys).toContain('KeyA')
-        expect(pressedKeys).toContain('KeyB')
-        expect(pressedKeys.length).toBe(2)
-    })
-
-
-    test('repeat flag', () => {
-        const listener = vi.fn()
-        keyboardDevice.on('keydown', listener)
-
-        window.dispatchEvent(new KeyboardEvent('keydown', {
-            code: 'KeyC',
-            key: 'c',
-            repeat: false
-        }))
-
-        window.dispatchEvent(new KeyboardEvent('keydown', {
-            code: 'KeyC',
-            key: 'c',
-            repeat: true
-        }))
-
-        expect(listener).toHaveBeenCalledTimes(2)
-        expect(listener.mock.calls[0][0].repeat).toBe(false)
-        expect(listener.mock.calls[1][0].repeat).toBe(true)
+        device.on('control:pressed', controlPressedListener)
+        device.on('control:released', controlReleasedListener)
         
-        const control = keyboardDevice.getControl('KeyC')
-        expect(control.isPressed()).toBe(true)
-    })
-
-
-    test('multiple modifiers', () => {
-        const keydownListener = vi.fn()
-        keyboardDevice.on('keydown', keydownListener)
-
-        const event = new KeyboardEvent('keydown', {
-            code: 'ControlLeft',
-            key: 'Control',
-            ctrlKey: true
-        })
-
-        Object.defineProperty(event, 'getModifierState', {
-            value: (key) => key === 'Control' || key === 'Shift'
-        })
+        device.start()
         
-        window.dispatchEvent(event)
-
-        expect(keydownListener).toHaveBeenCalled()
-        const keyState = keydownListener.mock.calls[0][0]
-        expect(keyState.modifiers.Control).toBe(true)
-        expect(keyState.modifiers.Shift).toBe(true)
+        const keydownListener = mockContainer.addEventListener.mock.calls
+            .find(call => call[0] === 'keydown')[1]
+        const keyupListener = mockContainer.addEventListener.mock.calls
+            .find(call => call[0] === 'keyup')[1]
         
-        expect(keyboardDevice.isKeyModifierPressed('Control')).toBe(true)
-        expect(keyboardDevice.isKeyModifierPressed('Shift')).toBe(true)
-    })
-
-
-    test('isKeyPressed returns boolean', () => {
-        expect(typeof keyboardDevice.isKeyPressed('NonExistentKey')).toBe('boolean')
+        const event = {code: 'Space'}
         
-        window.dispatchEvent(new KeyboardEvent('keydown', {
-            code: 'KeyD',
-            key: 'd'
-        }))
+        // Press
+        keydownListener(event)
+        expect(controlPressedListener).toHaveBeenCalledTimes(1)
+        expect(device.isPressed('Space')).toBe(true)
         
-        expect(typeof keyboardDevice.isKeyPressed('KeyD')).toBe('boolean')
-    })
-
-
-    test('isKeyModifierPressed returns boolean', () => {
-        expect(typeof keyboardDevice.isKeyModifierPressed('NonExistentModifier')).toBe('boolean')
+        // Release
+        keyupListener(event)
+        expect(controlReleasedListener).toHaveBeenCalledTimes(1)
+        expect(device.isPressed('Space')).toBe(false)
     })
 
 })
