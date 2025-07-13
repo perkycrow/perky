8d11ddf43a3e4269f7bb5650d1a7cc397ea3f98c
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2025-07-13 16:29:05 +0200

    fixed lit warnings

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,17 @@
 import {defineConfig} from 'vitest/config'
 
 
+const ignoredMessages = [
+    'Lit is in dev mode.'
+]
+
 export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
-        include: ['**/*.test.js', '**/*.spec.js']
+        include: ['**/*.test.js', '**/*.spec.js'],
+        onConsoleLog (log) {
+            return !ignoredMessages.some(message => log.includes(message))
+        }
     }
 })
