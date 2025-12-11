2347e5d747d5c8a912469565b7eefb262dc9ac70
hugeen <bogaert.cyrille@gmail.com>
2025-12-11 18:05:44 +0100

    fixed defend_the_den

--- defend_the_den/controllers/game_controller.js
+++ defend_the_den/controllers/game_controller.js
@@ -5,23 +5,22 @@ export default class GameController extends ActionController {
 
 
     move (direction, deltaTime) {
-        this.get('player').y += direction.y * 3 * deltaTime
+
     }
 
     moveUp () {
-        this.set('moving', 'up')
+
     }
 
     moveDown () {
-        this.set('moving', 'down')
+
     }
 
     stopMove () {
-        this.set('moving', null)
+
     }
 
     shoot () {
-        console.log('Shoot', Date.now())
         this.emit('shoot')
     }
 
