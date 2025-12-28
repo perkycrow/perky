48ea080dc9e0ae6399a22ef0fd75337706dc6526
hugeen <bogaert.cyrille@gmail.com>
2025-12-28 12:00:49 +0100

    removed three

--- three/effects/index.js
+++
@@ -1,4 +0,0 @@
-export {default as RenderComposer} from './render_composer'
-export {default as VignettePass} from './vignette_pass'
-export {default as AmberLUTPass} from './amber_lut_pass'
-export {default as CRTPass} from './crt_pass' 
