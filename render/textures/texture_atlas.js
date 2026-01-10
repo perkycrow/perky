4087967ebfd905fd6947d449fda5a7e37a7f961e
hugeen <bogaert.cyrille@gmail.com>
2026-01-10 02:50:18 +0100

    added shelf packer

--- render/textures/texture_atlas.js
+++ render/textures/texture_atlas.js
@@ -1,4 +1,5 @@
 import TextureRegion from './texture_region.js'
+import ShelfPacker from './shelf_packer.js'
 
 
 const DEFAULT_SIZE = 2048
@@ -116,66 +117,6 @@ export default class TextureAtlas {
 
     static get MAX_SIZE () {
         return MAX_SIZE
-    }
-
-}
-
-
-class ShelfPacker {
-
-    constructor (width, height, padding = 1) {
-        this.width = width
-        this.height = height
-        this.padding = padding
-        this.shelves = []
-        this.currentY = 0
-        this.full = false
-    }
-
-
-    pack (w, h) {
-        const pw = w + this.padding
-        const ph = h + this.padding
-
-        for (const shelf of this.shelves) {
-            if (shelf.height >= ph && shelf.remainingWidth >= pw) {
-                const slot = {x: shelf.x, y: shelf.y}
-                shelf.x += pw
-                shelf.remainingWidth -= pw
-                return slot
-            }
-        }
-
-        if (this.currentY + ph > this.height) {
-            this.full = true
-            return null
-        }
-
-        const shelf = {
-            y: this.currentY,
-            x: pw,
-            height: ph,
-            remainingWidth: this.width - pw
-        }
-
-        this.shelves.push(shelf)
-        this.currentY += ph
-
-        return {x: 0, y: shelf.y}
-    }
-
-
-    canFit (w, h) {
-        const pw = w + this.padding
-        const ph = h + this.padding
-
-        for (const shelf of this.shelves) {
-            if (shelf.height >= ph && shelf.remainingWidth >= pw) {
-                return true
-            }
-        }
-
-        return this.currentY + ph <= this.height
     }
 
 }
