d9ad45beb797d5eb9bbce767396dfafb83fa919f
hugeen <bogaert.cyrille@gmail.com>
2026-01-27 17:51:37 +0100

    added compression

--- io/pack.js
+++ io/pack.js
@@ -1,4 +1,16 @@
-export async function pack (files) {
+async function compress (blob) {
+    const stream = blob.stream().pipeThrough(new CompressionStream('gzip'))
+    return new Response(stream).blob()
+}
+
+
+async function decompress (blob) {
+    const stream = blob.stream().pipeThrough(new DecompressionStream('gzip'))
+    return new Response(stream).blob()
+}
+
+
+export async function pack (files, { compress: shouldCompress = true } = {}) {
     const header = {
         files: files.map(f => ({
             name: f.name,
@@ -10,7 +22,9 @@ export async function pack (files) {
     const headerBytes = new TextEncoder().encode(JSON.stringify(header))
     const headerSize = new Uint32Array([headerBytes.length])
 
-    return new Blob([headerSize, headerBytes, ...files.map(f => f.blob)])
+    const blob = new Blob([headerSize, headerBytes, ...files.map(f => f.blob)])
+
+    return shouldCompress ? compress(blob) : blob
 }
 
 
@@ -27,16 +41,17 @@ function blobToArrayBuffer (blob) {
 }
 
 
-export async function unpack (blob) {
-    const buffer = await blobToArrayBuffer(blob)
+export async function unpack (blob, { compressed = true } = {}) {
+    const data = compressed ? await decompress(blob) : blob
+    const buffer = await blobToArrayBuffer(data)
     const headerSize = new Uint32Array(buffer, 0, 1)[0]
     const headerBytes = new Uint8Array(buffer, 4, headerSize)
     const header = JSON.parse(new TextDecoder().decode(headerBytes))
 
     let offset = 4 + headerSize
     return header.files.map(file => {
-        const data = new Blob([buffer.slice(offset, offset + file.size)], {type: file.type})
+        const fileBlob = new Blob([buffer.slice(offset, offset + file.size)], {type: file.type})
         offset += file.size
-        return {name: file.name, blob: data}
+        return {name: file.name, blob: fileBlob}
     })
 }
