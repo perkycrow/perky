8c3fc49e5a490f21efa1d93d7da472fb9182ead1
hugeen <bogaert.cyrille@gmail.com>
2026-03-28 16:55:24 +0100

    added mist

--- vite.mist.config.js
+++ vite.mist.config.js
@@ -1,24 +1,58 @@
 import {defineConfig} from 'vite'
 import path from 'path'
+import {cpSync} from 'fs'
 import {createStudioPlugin} from './vite_helpers.js'
 import perkyConfig from './mist/perky.config.js'
 
 
-export default defineConfig(() => {
+export default defineConfig(({mode}) => {
+    const gameOnly = mode === 'game'
+
+    const input = {
+        main: path.resolve(import.meta.dirname, 'mist/index.html')
+    }
+
+    const plugins = [{
+        name: 'copy-assets',
+        closeBundle () {
+            cpSync(
+                path.resolve(import.meta.dirname, 'mist/assets'),
+                path.resolve(import.meta.dirname, 'dist/mist/assets'),
+                {recursive: true}
+            )
+        }
+    }]
+
+    if (!gameOnly) {
+        input.studio = path.resolve(import.meta.dirname, 'mist/studio/index.html')
+        input.animator = path.resolve(import.meta.dirname, 'mist/studio/animator.html')
+        input.spritesheet = path.resolve(import.meta.dirname, 'mist/studio/spritesheet.html')
+        input.scene = path.resolve(import.meta.dirname, 'mist/studio/scene.html')
+
+        plugins.unshift(createStudioPlugin({
+            game: 'mist',
+            ...perkyConfig.studio
+        }))
+    }
+
     return {
         root: './mist',
         base: './',
         publicDir: false,
-        plugins: [
-            createStudioPlugin({
-                game: 'mist',
-                ...perkyConfig.studio
-            })
-        ],
+        build: {
+            outDir: '../dist/mist',
+            emptyOutDir: true,
+            assetsDir: 'js',
+            minify: false,
+            rollupOptions: {
+                input
+            }
+        },
         resolve: {
             alias: {
                 perky: path.resolve(import.meta.dirname, './')
             }
-        }
+        },
+        plugins
     }
 })
